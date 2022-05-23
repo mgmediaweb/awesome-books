fb28d0340c1fe050a6a5b61104f3e24e235a9678
Gonzalo <mgmediaweb@gmail.com>
2022-05-23 12:04:54 -0700

    add and list book done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,82 @@
-const addBooks = () => {
-  /*
-  const bookInfo = {
-      first_name: document.getElementById('first_name').value,
-  };
+const form = document.getElementById('addBookForm');
 
-  localStorage.setItem('books', JSON.stringify(bookInfo));
-  */
+const setBooks = (books) => {
+  localStorage.setItem('books', JSON.stringify(books));
 };
 
-const getBooks = () => {
+const showBooks = (books) => {
+  const tableBook = document.getElementById('bookList');
+
+  books.forEach((item, key) => {
+    const newRow = tableBook.insertRow(key);
+    const newCellTitle = newRow.insertCell(0);
+    const newCellAuthor = newRow.insertCell(1);
+    const newCellButton = newRow.insertCell(2);
+
+    const newBook = document.createTextNode(item.title);
+    const newAuthor = document.createTextNode(item.author);
+
+    newCellTitle.appendChild(newBook);
+    newCellAuthor.appendChild(newAuthor);
+    newCellButton.innerHTML = `<button type="button" onClick="delBooks(${key})">Remove</button>`;
+  });
+
+  return true;
+};
+
+const getBooks = (opc) => {
   const books = JSON.parse(localStorage.getItem('books'));
 
   if (books) {
-    /*
-      document.getElementById('first_name').value = formData.first_name;
-      */
+    if (opc === 'get') {
+      return books;
+    }
+    showBooks(books);
+  }
+
+  return [];
+};
+
+const delBooks = (id = null) => {
+  if (id != null) {
+    const books = getBooks('get');
+
+    const newBooks = books.filter((item, key) => {
+      /* add code here */
+    });
+
+    setBooks(newBooks);
+    showBooks(newBooks);
   }
 };
 
-addBooks();
-getBooks();
+const addBooks = (title = null, author = null) => {
+  if (title && author) {
+    const bookInfo = {
+      title,
+      author,
+    };
+
+    const bookMem = getBooks('get');
+    bookMem.push(bookInfo);
+    setBooks(bookMem);
+  }
+
+  getBooks('show');
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const bookTitle = document.getElementById('book-title');
+  const bookAuthor = document.getElementById('book-author');
+
+  if ((bookTitle.value.trim() !== '') && (bookAuthor.value.trim() !== '')) {
+    addBooks(bookTitle.value, bookAuthor.value);
+    bookTitle.value = '';
+    bookAuthor.value = '';
+  }
+});
+
+delBooks();
+getBooks('show');

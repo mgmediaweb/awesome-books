d58167f24f2fb333aafb13c31b2b8bf6e6215e9a
Gonzalo <mgmediaweb@gmail.com>
2022-05-23 22:37:50 -0700

    classes done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,80 +1,90 @@
 const form = document.getElementById('addBookForm');
 const alert = document.querySelector('.alert');
 
-const hideAlert = () => {
-  alert.style.display = 'none';
-};
-
-const setBooks = (books) => {
-  localStorage.setItem('books', JSON.stringify(books));
-};
-
-const getBooks = () => {
-  const books = JSON.parse(localStorage.getItem('books'));
-  if (books) return books;
-  return [];
-};
-
-const showBooks = () => {
-  const books = getBooks();
-  const tableBook = document.getElementById('bookList').getElementsByTagName('tbody')[0];
-  tableBook.innerHTML = '';
-
-  if (books && books.length) {
-    books.forEach((item, key) => {
-      const newRow = tableBook.insertRow(key);
-      const newCellTitle = newRow.insertCell(0);
-      const newCellAuthor = newRow.insertCell(1);
-      const newCellButton = newRow.insertCell(2);
-
-      newCellButton.setAttribute('class', 'cellBtn text-center');
-
-      const newBook = document.createTextNode(item.title);
-      const newAuthor = document.createTextNode(item.author);
-
-      newCellTitle.appendChild(newBook);
-      newCellAuthor.appendChild(newAuthor);
-      newCellButton.innerHTML = `<button type="button" onClick="delBooks(${key})">Remove</button>`;
-    });
-  } else {
-    const newRow = tableBook.insertRow(0);
-    const newCell = newRow.insertCell(0);
-    const text = document.createTextNode('No books availables');
-
-    newCell.setAttribute('colspan', 3);
-    newCell.setAttribute('class', 'text-center');
-    newCell.appendChild(text);
+class BooksClass {
+  constructor() {
+    return null;
   }
 
-  return true;
-};
+  add(title = null, author = null) {
+    if (title && author) {
+      const bookInfo = {
+        title,
+        author,
+      };
 
-const delBooks = (id = null) => {
-  if (id != null) {
-    const books = getBooks();
+      const bookMem = this.get();
+      bookMem.push(bookInfo);
+      this.set(bookMem);
+      this.show();
+    }
+  }
+
+  del(id) {
+    const books = this.get();
 
     const newBooks = books.filter((item, key) => {
       if (key !== id) return true;
       return null;
     });
 
-    setBooks(newBooks);
-    showBooks();
+    //this.set(newBooks);
+    this.show();
   }
+
+  static get() {
+    const books = JSON.parse(localStorage.getItem('books'));
+    if (books) return books;
+    return [];
+  }
+
+  static set(books) {
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  show() {
+    const books = this.get();
+    const tableBook = document.getElementById('bookList').getElementsByTagName('tbody')[0];
+    tableBook.innerHTML = '';
+
+    if (books && books.length) {
+      books.forEach((item, key) => {
+        const newRow = tableBook.insertRow(key);
+        const newCellTitle = newRow.insertCell(0);
+        const newCellAuthor = newRow.insertCell(1);
+        const newCellButton = newRow.insertCell(2);
+
+        newCellButton.setAttribute('class', 'cellBtn text-center');
+
+        const newBook = document.createTextNode(item.title);
+        const newAuthor = document.createTextNode(item.author);
+
+        newCellTitle.appendChild(newBook);
+        newCellAuthor.appendChild(newAuthor);
+        newCellButton.innerHTML = `<button type="button" onClick="delBooks(${key})">Remove</button>`;
+      });
+    } else {
+      const newRow = tableBook.insertRow(0);
+      const newCell = newRow.insertCell(0);
+      const text = document.createTextNode('No books availables');
+
+      newCell.setAttribute('colspan', 3);
+      newCell.setAttribute('class', 'text-center');
+      newCell.appendChild(text);
+    }
+
+    return true;
+  }
+}
+
+const books = new BooksClass();
+
+const hideAlert = () => {
+  alert.style.display = 'none';
 };
 
-const addBooks = (title = null, author = null) => {
-  if (title && author) {
-    const bookInfo = {
-      title,
-      author,
-    };
-
-    const bookMem = getBooks();
-    bookMem.push(bookInfo);
-    setBooks(bookMem);
-    showBooks();
-  }
+const delBooks = (id = null) => {
+  if (id != null) books.del(id);
 };
 
 form.addEventListener('submit', (event) => {
@@ -84,7 +94,7 @@ form.addEventListener('submit', (event) => {
   const bookAuthor = document.getElementById('book-author');
 
   if ((bookTitle.value.trim() !== '') && (bookAuthor.value.trim() !== '')) {
-    addBooks(bookTitle.value, bookAuthor.value);
+    books.add(bookTitle.value, bookAuthor.value);
     bookTitle.value = '';
     bookAuthor.value = '';
   } else {
@@ -92,6 +102,6 @@ form.addEventListener('submit', (event) => {
   }
 });
 
-delBooks();
-showBooks();
+books.show();
 hideAlert();
+delBooks();
